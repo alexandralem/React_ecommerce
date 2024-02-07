5d72181ec3c53bc5197306c6242ba57d8995baf8
Alexandra Lem <a_lem@fanshaweonline.ca>
2024-02-07 21:12:40 +0600

    adding context

---
+++ ecommerce/src/context/user.context.jsx
@@ -0,0 +1,25 @@
+import { createContext, useState, useEffect } from "react";
+
+import { onAuthStateChangedListener, createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
+
+// as the actual value you want to access
+export const UserContext = createContext({
+    currentUser: null, //empty state of the object is null
+    setCurrentUser: () => {}
+})
+// the actual component
+export const UserProvider = ({children}) => {
+    const [currentUser, setCurrentUser] = useState(null);
+    const value = {currentUser, setCurrentUser}
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangedListener((user) => {
+            if(user) {
+                createUserDocumentFromAuth(user);
+            }
+            setCurrentUser(user);
+        });
+        return unsubscribe;
+    }, [])
+    return <UserContext.Provider value={value}>{children}</UserContext.Provider>
+}
--- ecommerce/src/routes/navigation/navigation.component.jsx
+++ ecommerce/src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,18 @@
-import { Fragment } from 'react' //Fragment is a wrapping div
+import { Fragment, useContext } from 'react' //Fragment is a wrapping div
 
 import {Link, Outlet} from 'react-router-dom' // Link is an anchor tag <a>
 
 import { ReactComponent as CrwnLogo} from '../../assets/crown.svg'
+import { UserContext } from '../../context/user.context'
+
+import { signOutUser } from '../../utils/firebase/firebase.utils'
 
 import './navigation.styles.scss'
 
 
 const Navigation = () => {
+    const {currentUser} = useContext(UserContext);
+
     return (
         <Fragment>
             <div className='navigation'>
@@ -19,9 +24,14 @@ const Navigation = () => {
                     <Link className='nav-link' to='/shop'>
                         SHOP
                     </Link>
-                    <Link className='nav-link' to='/sign-in'>
-                        SIGN IN
-                    </Link>
+                    {currentUser ? (
+                        <span className='nav-link' onClick={signOutUser}>SIGN OUT</span>
+                    ) : (
+                        <Link className='nav-link' to='/sign-in'>
+                            SIGN IN
+                        </Link>
+                    )}
+                    
                 </div>
             </div>
             <Outlet></Outlet>
